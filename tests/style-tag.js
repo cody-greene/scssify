b187f5053e6c6ff3082ab7f58842c60f5650423e
Cody Greene <cody-greene@users.noreply.github.com>
2017-04-27 10:30:24 -0700

    add test for autoInject.prepend

--- tests/style-tag.js
+++ tests/style-tag.js
@@ -12,3 +12,17 @@ it('supports <style> tags', integration(__dirname + '/util/basic.scss', {
   assert.equal(applied.color, 'red', 'text color changed')
   done()
 }))
+
+it('can prepend to the <head>', integration(__dirname + '/util/basic.scss', {
+  autoInject: {prepend: true}
+}, function (ctx, done) {
+  let children = ctx.document.head.childNodes
+  let dummy = children[1]
+  assert.equal(children.length, 2)
+  assert.equal(dummy.id, 'foo')
+  done()
+}, function (ctx) {
+  let dummy = ctx.document.createElement('style')
+  dummy.id = 'foo'
+  ctx.document.head.appendChild(dummy)
+}))
